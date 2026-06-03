9e6477e176a33676799268a81e0f042ea7b154c3
Surya Maulana Akhmad <129302343+suryamaulana98@users.noreply.github.com>
2026-06-03 00:07:27 +0000

    Update content

--- materi/_build/html/_static/custom.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var introLinks = document.querySelectorAll(
-    '.bd-sidenav__home-link a.reference.internal[href$="intro.html"], .bd-sidenav__home-link a.reference.internal[href="./"]'
-  );
-
-  introLinks.forEach(function (link) {
-    link.textContent = "240411100160";
-  });
-});
